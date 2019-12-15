e82fb948c9f1c4b9dca3ca04279526cfb3740f18
Angie <arco527@gmail.com>
2019-12-15 16:35:38 -0500

    With if

--- solution.rb
+++ solution.rb
@@ -9,12 +9,22 @@
 # end
 
 
-require 'sinatra'
+# require 'sinatra'
 
+# get '/' do
+#   unless params[:nombre] != nil && params[:nombre].capitalize == "Juan"
+#     "Hola desconocido!"
+#   else
+#     "<h1>Hola #{params[:nombre]}</h1>"
+#   end
+# end
+
+require 'sinatra'
 get '/' do
-  unless params[:nombre] != nil && params[:nombre].capitalize == "Juan"
-    "Hola desconocido!"
-  else
-    "<h1>Hola #{params[:nombre]}</h1>"
-  end
+if params[:nombre] == nil || params[:nombre]==""
+"<h1>Hola desconocido!</h1>"
+else
+
+"<h1>Hola #{params[:nombre]}!</h1>"
+end
 end
