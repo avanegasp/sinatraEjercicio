fa3fb4f329f855a0d9763d01e68082308ebe82fa
Angie <arco527@gmail.com>
2019-12-15 16:26:31 -0500

    With unless

--- solution.rb
+++ solution.rb
@@ -1,9 +1,23 @@
-require "sinatra"
+# require "sinatra"
 
+# get '/' do
+# if params[:nombre] && params[:nombre]!= ""
+# "<h1>Hola #{params[:nombre]}!<h1>"
+# else
+# "<h1>Hola desconocido!<h1>"
+# end
+# end
+
+
+require 'sinatra' 
 get '/' do
-if params[:nombre] && params[:nombre]!= ""
-"<h1>Hola #{params[:nombre]}!<h1>"
+unless params[:nombre]
+  <<-HTML
+    <h1>Hola desconocido!</h1>
+  HTML
 else
-"<h1>Hola desconocido!<h1>"
+  <<-HTML
+    <h1>Hola #{params[:nombre]}#{"!"}</h1>
+  HTML
 end
 end
